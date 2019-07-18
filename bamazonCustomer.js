fe063d38f9db00cf57571906bcccc984b7624675
Jason Peng <jason.j.peng@gamil.com>
2019-07-18 15:38:42 -0700

    customer update sales in DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,7 +9,7 @@ const connectionParams = {
     database: "bamazon"
   };
 const arrPrompts = [
-  {message: "Please eneter the product Id:  ", type: 'input', name: 'id'},
+  {message: "\n\nPlease eneter the product Id:  ", type: 'input', name: 'id'},
   {message: "Please enter the quantity:  ", type: 'input', name: 'num'}
 ];  
 
@@ -32,33 +32,29 @@ async function showProducts(arrProducts) {
 }  
 
 async function updateProductNum(id, num) {
-    try {
-        // let conn = await mysql.createConnection(connectionParams);
+    try {      
         let [rows,fields] = await conn.query("SELECT * FROM products WHERE item_id = ?", [id]);
-        conn.end();
         if (rows.length !== 1) {
             console.log(`${id} is not a valid product Id.`);
             return 0;
-        } else {
-            var newNum = rows[0].stock_quantity - num;
-            var totalPrice = rows[0].price * num;
-            var productName = rows[0].product_name;
-            if (newNum < 0 ) {
-               console.log("Insufficient quantity!");
-               return 0;
-            } else {
-                try {
-                   let conn = await mysql.createConnection(connectionParams);
-                   let [rows,fields] = await conn.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?",
-                                                         [newNum, id]);
-                    console.log(`You bought ${num} ${productName} !  $${totalPrice} will be deducted from your account`);                                     
-                    conn.end();
-                } catch (err) {
-                    console.log(err);
-                }   
-            }
-        }        
+        } 
+        var newNum = rows[0].stock_quantity - num;
+        if (newNum < 0) {
+             console.log("Insufficient quantity!");
+             return  0;
+        }
+        var totalPrice = parseInt(rows[0].price) * num;
+        var productName = rows[0].product_name;
+        var newSales = parseInt(rows[0].product_sales) + totalPrice;
+        let res = await conn.query("UPDATE products SET ? WHERE item_id = ?",
+                                               [{  "stock_quantity": newNum,
+                                                   "product_sales" : newSales 
+                                                 }, 
+                                                    id]);
+        
+        console.log(`You bought ${num} ${productName} !  $${totalPrice} will be deducted from your account`);                                             
       } catch (err) {
+        console.log("ERROR: Sales NOT completed")
         console.log(err);
         return 0;
       };
